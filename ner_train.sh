796dc936ede7758070fece8b0b80139b017e8580
Chahyon Ku <ku000045@agc01.agate.msi.umn.edu>
2022-12-04 15:24:31 -0600

    ner_train.sh

---
+++ ner_train.sh
@@ -0,0 +1,17 @@
+#python ner_train.py --num_workers 8 --train_data data/en-asal.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/base_en-asal_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/en-ku.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/base_en-ku_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/en-london.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/base_en-london_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/fr-asal.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/base_fr-asal_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/fr-ku.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/base_fr-ku_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/fr-london.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/base_fr-london_20 --n_epochs 20
+
+
+#python ner_train.py --num_workers 8 --train_data data/en-orig-london.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/transv2/base_en-orig-london_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/fr-orig-london.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/transv2/base_fr-orig-london_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/MultiCoNER2/en-train.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/transv2/base_en_20 --n_epochs 20
+#python ner_train.py --num_workers 8 --train_data data/MultiCoNER2/fr-train.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/transv2/base_fr_20 --n_epochs 20
+
+python ner_train.py --num_workers 8 --train_data data/en-melm.conll --valid_data data/MultiCoNER2/en-dev.conll --log_dir ./logs/transv2/base_en-melm_20 --n_epochs 20
+python ner_train.py --num_workers 8 --train_data data/fr-melm.conll --valid_data data/MultiCoNER2/fr-dev.conll --log_dir ./logs/transv2/base_fr-melm_20 --n_epochs 20
+
+
